41d4784024eba84189e3a9373294c08d70da883c
John Maguire <john.maguire@sap.com>
2015-01-19 09:38:54 +0000

    building multiple bundles

---
+++ app/vibratular/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jonnie on 16 Jan 2015.
+ */
+function xy(){
+
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,84 @@
-var gulp = require('gulp'),
-	connect = require('gulp-connect');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+var browserify = require('browserify');
 
-gulp.task('webserver', function() {
-	connect.server({
-		root: 'app',
-		livereload: true
+clean = require('gulp-clean');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var bundler = watchify(browserify('./app/index.js', watchify.args));
+// add any other browserify options or transforms here
+//bundler.transform('brfs');
+
+gulp.task('js', jsbundle); // so you can run `gulp js` to build the file
+bundler.on('update', jsbundle); // on any dep update, runs the bundler
+
+function jsbundle() {
+	return bundler.bundle()
+		// log errors if they happen
+		.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+		.pipe(source('bundle.js'))
+		// optional, remove if you dont want sourcemaps
+		.pipe(buffer())
+		.pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+		.pipe(sourcemaps.write('./')) // writes .map file
+		//
+		.pipe(gulp.dest('./dev'));
+}
+
+var htmlFiles = ['app/index.html']
+	gulp.task('html', function(){
+		// the base option sets the relative root for the set of files,
+		// preserving the folder structure
+		gulp.src(htmlFiles, { base: './app' })
+			.pipe(gulp.dest('dev/'));
 	});
+
+
+// watch files for changes and reload
+gulp.task('serve', ['js2', 'html'], function() {
+	browserSync({
+		server: {
+			baseDir: 'dev'
+		}
+	});
+
+
+	gulp.watch(['dev/**/*'], {cwd: 'dev'}, reload);
 });
 
-gulp.task('default', ['webserver']);
+var appBundler = watchify(browserify(['./app'], watchify.args));
+appBundler.on('update',  function(){ createBundle({ bundle: appBundler, name:'bundle.js', dest: './dev'})});
+
+var vbBundler = watchify(browserify(['./app/vibratular'], watchify.args));
+vbBundler.on('update', function(){ createBundle({bundle: vbBundler, name:'vibratular.js', dest: './dev'})});
+
+var bundlers = [{ bundle: appBundler, name:'bundle.js', dest: './dev'},
+	{ bundle: vbBundler, name:'bundle.js', dest: './dev' }];
+
+function createBundle(bundler) {
+	return (function() {
+		console.log('Attempting Update');
+		bundler.bundle.bundle()
+			// log errors if they happen
+			.on('error', gutil.log.bind(gutil, 'Browserify Error'))
+			.pipe(source(bundler.name))
+			// optional, remove if you dont want sourcemaps
+			.pipe(buffer())
+			.pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+			.pipe(sourcemaps.write('./')) // writes .map file
+			//
+			.pipe(gulp.dest(bundler.dest));
+	})();
+};
+gulp.task('js2', function(){
+	bundlers.forEach(function(bundler){
+		createBundle(bundler);
+	});
+
+});
